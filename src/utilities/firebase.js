f52da75089e21e740c5dc07dd3c7d0a851556cb2
Josh <joshualee721@gmail.com>
2022-04-03 15:21:51 -0500

    read from firebase

---
+++ src/utilities/firebase.js
@@ -0,0 +1,49 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBzQ8PfdOzHnOMK2MCgryfKbPswpMqUoIs",
+  
+    authDomain: "josh-scheduler-2d177.firebaseapp.com",
+  
+    databaseURL: "https://josh-scheduler-2d177-default-rtdb.firebaseio.com",
+  
+    projectId: "josh-scheduler-2d177",
+  
+    storageBucket: "josh-scheduler-2d177.appspot.com",
+  
+    messagingSenderId: "616089150553",
+  
+    appId: "1:616089150553:web:dac2c560131f04b481fd48",
+  
+    measurementId: "G-7M6YN7X8FG"
+  
+  };
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      const dbRef = ref(database, path);
+      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      if (devMode) { console.log(`loading ${path}`); }
+      return onValue(dbRef, (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) { console.log(val); }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      }, (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      });
+    }, [path, transform]);
+  
+    return [data, loading, error];
+  };
